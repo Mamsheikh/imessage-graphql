73fd7a15d3b07bb652b4996a1dd23abb37826660
Munir Ali Mohammed <mamsheikh123@gmail.com>
2022-09-29 21:42:22 +0100

    Authentication
    Google Authentication using Next-Auth and Prisma Setup

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,7 +1,14 @@
 import type { AppProps } from 'next/app';
+import { SessionProvider } from 'next-auth/react';
+import { Session } from 'next-auth';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+
+function MyApp({ Component, pageProps }: AppProps<{session:Session}>) {
+  return (
+    <SessionProvider session={pageProps.session}>
+      <Component {...pageProps} />;
+    </SessionProvider>
+  );
 }
 
 export default MyApp;
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,7 +1,20 @@
 import type { NextPage } from 'next';
+import { useSession, signOut, signIn } from 'next-auth/react';
 
 const Home: NextPage = () => {
-  return <div>Hello World!</div>;
+  const { data: session } = useSession();
+  console.log(session);
+
+  return (
+    <div>
+      {session?.user ? (
+        <button onClick={() => signOut()}>Logout</button>
+      ) : (
+        <button onClick={() => signIn('google')}>Login</button>
+      )}
+      {session?.user?.email}
+    </div>
+  );
 };
 
 export default Home;
