adc91927d17d4624b4db5b1c30eba247d5ee9126
Munir Ali Mohammed <mamsheikh123@gmail.com>
2022-10-05 17:18:15 +0100

    fix:graphql folders

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -3,7 +3,7 @@ import { SessionProvider } from 'next-auth/react';
 import { Session } from 'next-auth';
 import { ChakraProvider } from '@chakra-ui/react';
 import { theme } from '../chakra/theme';
-import { client } from '../graphq/apollo-client';
+import { client } from '../graphql/apollo-client';
 import { ApolloProvider } from '@apollo/client';
 import { Toaster } from 'react-hot-toast';
 
