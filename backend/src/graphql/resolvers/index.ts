c866afff6eda10bc44a07e2f856ecbf790bb23a0
Munir Ali Mohammed <mamsheikh123@gmail.com>
2022-12-24 20:26:09 +0100

    feat:add scalar dateTime for graphql

--- backend/src/graphql/resolvers/index.ts
+++ backend/src/graphql/resolvers/index.ts
@@ -1,8 +1,15 @@
 import userResolvers from './user';
 import conversationResolver from './conversation';
-import messageResolvers from './message'
+import messageResolvers from './message';
+import dateResolvers from './scalars';
 import merge from 'lodash.merge';
 
-const resolvers = merge({}, userResolvers, conversationResolver, messageResolvers);
+const resolvers = merge(
+  {},
+  userResolvers,
+  conversationResolver,
+  messageResolvers,
+  dateResolvers
+);
 
 export default resolvers;
